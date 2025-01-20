781caca4375f5898b6497623f08023772e2a0985
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-20 15:34:59 +0530

    node server Hello World

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>');
+});
+
 app.listen(8000, () => {
   console.log('Server is running on http://localhost:8000');
 });
