c1f7bf4c17bf3ea58fb577094a670033518c9e4b
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-20 15:45:11 +0530

    Update server response format and add about route localhost:8000/about

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.send({message:'<h1>Hello World</h1>'});
+    res.send('<h1>Hello World</h1>');
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About</h1>');
 });
 
 app.listen(8000, () => {
