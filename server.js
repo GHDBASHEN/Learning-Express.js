cc42e63ccc0ed6615247bf7ffae42b4499d613f7
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-20 22:51:20 +0530

    Add API endpoint to retrieve all posts and a single post by ID

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const posts = [
     {id: 2, title: 'Post 2', content: 'This is the second post'},
     {id: 3, title: 'Post 3', content: 'This is the third post'}
 ];
-
+//get all posts
 app.get('/api/posts', (req, res) => {
     console.log(req.query);
     res.json(posts);
 });
-
+//get single post
 app.get('/api/posts/:id', (req, res) => {
     const id =parseInt(req.params.id);
     res.json(posts.filter(post => post.id === id));
