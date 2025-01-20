5c6bff20a081c1b7e4354e8a105a633f4818f48e
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-20 20:05:41 +0530

    res.sendFile() Method

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+    res.sendFile(path.join(__dirname ,'public', '/index.html'));
 });
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>');
+    res.sendFile(path.join(__dirname ,'public', '/about.html'));
 });
 
 app.listen(8000, () => {
