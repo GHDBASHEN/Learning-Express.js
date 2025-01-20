9626d2e3122d440957242231b8cb7e443028e791
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-20 22:17:22 +0530

    Update development script to use .env file and change server port to 3000

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.get('/api/posts', (req, res) => {
 
 
 app.listen(port, () => {
-  console.log('Server is running on http://localhost:8000');
+  console.log('Server is running on http://localhost:3000');
 });
