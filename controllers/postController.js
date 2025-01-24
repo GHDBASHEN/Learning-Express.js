70b91565698e5a0e3f7259c8ff0950dae78475b7
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-24 22:36:04 +0530

    Add post submission form and functionality to create new posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,7 @@
 let posts = [
-    { id: 1, title: 'Post 1', content: 'This is the first post' },
-    { id: 2, title: 'Post 2', content: 'This is the second post' },
-    { id: 3, title: 'Post 3', content: 'This is the third post' },
+    { id: 1, title: 'Post 1'},
+    { id: 2, title: 'Post 2'},
+    { id: 3, title: 'Post 3'},
   ];
   
 
