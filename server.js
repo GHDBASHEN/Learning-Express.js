96232574ffa8a148e994041d6061170f049e020a
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-24 12:26:27 +0530

    Add static file serving and directory path resolution in server setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import posts from './routes/posts.js';
 import errorhandler from './middleware/error.js';
 import notFound from './middleware/notFound.js';
@@ -7,11 +8,19 @@ import logger from './middleware/logger.js';
 
 const port = process.env.PORT || 3000;
 const app = express();
+//get directory name
+const __filename = fileURLToPath(import.meta.url);
+console.log(__filename);
+const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//setup static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Routes
 app.use('/api/posts', posts);
